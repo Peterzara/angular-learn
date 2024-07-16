1d14f5c8327fd36019441a1dfb229ec18972e41d
Peterzara <76547899+Peterzara@users.noreply.github.com>
2024-07-16 17:53:21 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-learn
+
+[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/Peterzara/angular-learn)
---
+++ src/app/product-details/product-details.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+import { Product, products } from '../products';
+
+@Component({
+  selector: 'app-product-details',
+  // standalone: true,
+  // imports: [],
+  templateUrl: './product-details.component.html',
+  styleUrl: './product-details.component.css',
+})
+export class ProductDetailsComponent implements OnInit {
+  product: Product | undefined;
+
+  constructor(private route: ActivatedRoute) {}
+
+  ngOnInit() {
+    // Get the product id from the current route.
+    const routeParams = this.route.snapshot.paramMap;
+    const productIdFromRoute = Number(routeParams.get('productId'));
+
+    this.product = products.find(
+      (product) => product.id === productIdFromRoute
+    );
+  }
+}
